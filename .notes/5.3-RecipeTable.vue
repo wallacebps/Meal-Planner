f29acf705b829877404e82efb52258bcbaa3e640
wallacebps <wallace.bruno@geocode.com.br>
2024-03-20 10:00:25 -0300

    Revert "remove some trash"
    This reverts commit 4a89027579d4f176ce43fb4ef576e88ea614d9ba.

--- .notes/5.3-RecipeTable.vue
+++ .notes/5.3-RecipeTable.vue
@@ -1,26 +1,15 @@
 <script setup lang="ts">
-// import { usePlannerStore } from "@/stores/modules/planner";
 const emits = defineEmits(["openPreview"]);
-
-// const store = usePlannerStore();
 
 import type { Recipe } from "@/types/spoonacular";
 interface RecipeList extends Recipe {
   date: Date;
 }
 
-// import ItemRating from "./ItemRating.vue";
-
 const props = defineProps<{
   recipes: RecipeList[];
   title: string;
-//   removable?: boolean; 
 }>();
-
-// const removeFromDay = (recipes: { id: number; date: Date }): void => {
-//   const { id, date } = recipes;
-//   store.removeRecipeByIdDate({ id, date });
-// };
 
 const openPreview = (recipe: Recipe): void => {
   emits("openPreview", recipe);
@@ -39,14 +28,8 @@ const openPreview = (recipe: Recipe): void => {
           <v-sheet class="d-flex justify-space-between">
             <v-sheet class="ma-2 pa-2">
               <h2 class="text-h4">{{ recipe.title }}</h2>
-              <!-- <ItemRating :id="recipe.id" /> -->
             </v-sheet>
             <v-sheet class="ma-2 pa-2">
-              <!-- <v-btn
-                v-if="removable"
-                @click="removeFromDay({ id: recipe.id, date: recipe.date })"
-                text-color="red"
-                icon="mdi-trash-can-outline" class="mr-4"></v-btn> -->
               <v-btn
                 @click="openPreview(recipe)"
                 text-color="light-green"
